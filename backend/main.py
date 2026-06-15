ac9777188d9803ccba31877900110e59fbe6cb42
Kitsune-28 <artem.gorbunov1728@icloud.com>
2026-06-15 12:57:13 +0900

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ allowed_origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allowed_origins = allowed_origins,
+    allow_origins = allowed_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
